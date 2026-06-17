d5d3305e1db20105f5a358180f1a3f5dbb9ee0a3
Jai Harsan S <jaiharsan.s2008@gmail.com>
2026-06-17 23:15:21 +0530

    Add category field to place model migration

--- places/migrations/0002_place_category_place_city.py
+++ places/migrations/0002_place_category_place_city.py
@@ -4,7 +4,11 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
+    dependencies = [
+        ('places', '0001_initial'),
+    ]
 
+    
     
     operations = [
         migrations.AddField(
