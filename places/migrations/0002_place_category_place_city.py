eacfc12f128343baa750c4b394eae61f577b0a8f
Jai Harsan S <jaiharsan.s2008@gmail.com>
2026-06-17 23:15:03 +0530

    Remove dependencies from place category migration

--- places/migrations/0002_place_category_place_city.py
+++ places/migrations/0002_place_category_place_city.py
@@ -5,10 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ('places', '0001_initial'),
-    ]
-
+    
     operations = [
         migrations.AddField(
             model_name='place',
