1efbd86edf69e624ed9821afff68c37d61dd53f8
Jai Harsan S <jaiharsan.s2008@gmail.com>
2026-06-30 23:08:11 +0530

    Remove unused Django ORM admin code
    Removed commented-out code for Django ORM integration.

--- places/admin.py
+++ places/admin.py
@@ -3,8 +3,7 @@ from django.contrib import admin
 # MongoEngine models don't work with Django admin
 # To manage MongoDB documents, use MongoDB Compass or pymongo scripts
 
-# Uncomment below if using Django ORM instead of MongoEngine:
-# from .models import Place
+
 # 
 # @admin.register(Place)
 # class PlaceAdmin(admin.ModelAdmin):
