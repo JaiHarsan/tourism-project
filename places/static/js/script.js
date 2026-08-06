f9412d48c1c14269bad0a38ac40fdcc68836f18c
Jai Harsan S <jaiharsan.s2008@gmail.com>
2026-08-06 23:55:49 +0530

    Remove extra newline in script.js

--- places/static/js/script.js
+++ places/static/js/script.js
@@ -5,7 +5,7 @@ const placesContainer = document.getElementById('placesContainer');
 const emptyState = document.getElementById('emptyState');
 const themeToggle = document.getElementById('themeToggle');
 const filterBtns = document.querySelectorAll('.filter-btn:not(.city-filter)');
-
+  
 let currentFilter = 'all';
 let currentSearch = '';
 let isSearching = false;
